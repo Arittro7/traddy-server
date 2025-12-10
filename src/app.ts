58faf57d5235d48be6f4efe1ae2ebdd5d67dbebe
Arittro7 <arittrowpdev@gmail.com>
2025-12-11 02:14:49 +0600

    Recover and Push to new Repo

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response } from "express";
+import routes from "./routes"; // Your main router (import all modules there)
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { notFoundRoute } from "./middleware/notFoundRoute";
+import dotenv from "dotenv";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
+import { PaymentController } from "./app/modules/payments/payments.controller";
+import { envVars } from "./app/config/env";
+
+// Load env vars
+dotenv.config();
+
+const app = express();
+app.use(cookieParser());
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  PaymentController.handleStripeWebhookEvent
+);
+
+// CORS configuration
+const corsOptions = {
+  origin: [
+    envVars.FRONTEND_URL,
+    // "https://frontend-url.vercel.app",
+    "http://localhost:3000",
+    /\.vercel\.app$/,
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
+// Landing page with Local Guide Platform API info
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Traddy - Travel Buddy Server Is Running`);
+});
+
+app.use(globalErrorHandler);
+app.use(notFoundRoute);
+
+export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import authRoute from "../app/modules/auth/auth.route";
+import userRoute from "../app/modules/users/users.route";
+import { listingRoute } from "../app/modules/listings/listings.route";
+import { bookingRoute } from "../app/modules/bookings/bookings.route";
+import { reviewRoute } from "../app/modules/reviews/reviews.route";
+import { wishlistRoutes } from "../app/modules/wishlist/wishlist.route";
+
+const routes = Router();
+
+routes.use("/auth", authRoute);
+routes.use("/user", userRoute);
+routes.use("/listing", listingRoute);
+routes.use("/booking", bookingRoute);
+routes.use("/review", reviewRoute);
+routes.use("/wishlist", wishlistRoutes);
+
+export default routes;
